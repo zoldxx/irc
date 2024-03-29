179a39ebe9ae54886c0d3a2f9f5fc78b8433af80
Thomas GEORGE <tgeorge@bess-f2r4s2.clusters.42paris.fr>
2024-03-29 14:30:43 +0100

    norminette invite

--- invite.cpp
+++ invite.cpp
@@ -1,72 +1,147 @@
 	#include "inc/server.hpp"
 
-bool	Server::invite(User & client, std::string cmd)
+bool	Server::initNickAndChan(std::string &nick, std::string &channel, User &client, std::string cmd)
 {
-	(void)client;
-	(void)cmd;
-	std::string					nick("");
-	std::string					channel("");
+	std::string					msg;
 
 	if (cmd.find(" ", 0) != std::string::npos)
 		nick = cmd.substr(0, cmd.find(" ", 0));
 	cmd.erase(0, cmd.find(" ", 0) + 1);
-	if (nick.size() == 0 || channel.size() == 0)
-		//Exception ou msg d'erreur ?
 	channel = cmd;
 	if (channel[0] != '#')
 	{
 		if (nick[0] != '#')
 		{
-			std::cout << "Wrong parameters for invite (to change to good error msg)" << std::endl;
+			std::cout << "HERE 1" << std::endl;
+			msg = ":localhost 401 " + client.getNick() + " " + channel + " :No such nick/channel\r\n";
+			if (send(client.getFd(), msg.c_str(), msg.size(), 0) < 1)
+			{
+				del_user(client.getFd());
+				return false;
+			}
 			return false;
 		}
 		else
 			std::swap(nick, channel);
 	}
-	std::map<std::string, Channel>::iterator it_channel;
-	it_channel = _channels.find(channel);
-	std::vector<int>::iterator it_oper;
-	if (it_channel != _channels.end())
-	{
-		for (it_oper = it_channel->second.getOperators().begin() ; it_oper != it_channel->second.getOperators().end() ; it_oper++)
-			if ((*it_oper) == client.getFd())
-				break;
-		if (it_oper == it_channel->second.getOperators().end())
-			//msg probleme pas operateur
-			;
-	}
-	else
-		//exception no nick/channel;
-		;
-	std::map<int, User>::iterator it_guest;
+	std::cout << "HERE 2" << std::endl;
+	channel = channel.erase(0, 1);
+	return true;
+}
+
+bool	Server::checkGuest(std::map<int, User>::iterator &it_guest, User & client, std::string &nick, std::string &channel)
+{
+	std::string					msg;
+
 	for (it_guest = _users.begin() ; it_guest != _users.end() ; it_guest++)
 	{
 		if (it_guest->second.getNick() == nick)
 			break;
 	}
 	if (it_guest == _users.end())
-		//send || :nonstop.ix.me.dal.net 401 finf #finf :No such nick/channel || to inviter;
-		;
-	std::vector<int>::iterator it_users;
-	std::vector<int>::iterator it_operators;
-	for (it_users = it_channel->second.getUsers().begin() ; it_users != it_channel->second.getUsers().end() ; it_users++)
 	{
-		if ((*it_users) == it_guest->second.getFd())
-			break;
+		std::cout << "HERE 3" << std::endl;
+		msg = ":localhost 401 " + client.getNick() + " " + channel + " :No such nick/channel\r\n";
+		if (send(client.getFd(), msg.c_str(), msg.size(), 0) < 1)
+		{
+			del_user(client.getFd());
+			return false;
+		}
+		return false;
 	}
-	for (it_operators = it_channel->second.getOperators().begin() ; it_operators != it_channel->second.getOperators().end() ; it_operators++)
+	std::cout << "HERE 4" << std::endl;
+	return true;
+}
+
+void	Server::addToWhitelist(std::map<std::string, Channel>::iterator &it_channel,
+			std::map<int, User>::iterator &it_guest, User & client, std::string channel)
+{
+	std::string	msg;
+
+	it_channel->second.getWhitelist().push_back(it_guest->second.getFd());
+	msg = ":localhost 341 " + client.getNick() + " " + it_guest->second.getNick() + " #" + channel + "\r\n";
+	if (send(client.getFd(), msg.c_str(), msg.size(), 0) < 1)
 	{
-		if ((*it_operators) == it_guest->second.getFd())
-			break;
+		del_user(client.getFd());
+		return ;
 	}
-	if (it_users == it_channel->second.getUsers().end() && it_operators == it_channel->second.getOperators().end())
+	msg = ":localhost NOTICE @#" + channel + " :" + client.getNick() + " invited " + it_guest->second.getNick() + " into channel " + " #" + channel + "\r\n";
+	if (send(client.getFd(), msg.c_str(), msg.size(), 0) < 1)
 	{
-		it_channel->second.getWhitelist().push_back(it_guest->second.getFd());
-		//>> :nonstop.ix.me.dal.net 341 finf finfin #finf
-		//>> :serenity.fl.us.dal.net NOTICE @#finf :finf invited finfin into channel #finf
+		del_user(client.getFd());
+		return ;
+	}
+	msg = ":" + client.getNick() + "!~" + client.getUsername() + "@localhost INVITE " + it_guest->second.getNick() + " :#" + channel + "\r\n";
+	if (send(it_guest->second.getFd(), msg.c_str(), msg.size(), 0) < 1)
+	{
+		del_user(it_guest->second.getFd());
+		return ;
+	}
+}
+
+bool	Server::invite(User & client, std::string cmd)
+{
+	std::string					nick("");
+	std::string					channel("");
+	std::string					msg;
+
+	if(!initNickAndChan(nick, channel, client, cmd))
+		return false;
+
+	std::map<std::string, Channel>::iterator it_channel = _channels.find(channel);
+	if (it_channel != _channels.end())
+	{
+		std::map<int, User>::iterator it_guest;
+		if (!checkGuest(it_guest, client, nick, channel))
+			return false;
+		std::vector<int>::iterator it_users;
+		std::vector<int>::iterator it_operators;
+		for (it_users = it_channel->second.getUsers().begin() ; it_users != it_channel->second.getUsers().end() ; it_users++)
+		{
+			if ((*it_users) == it_guest->second.getFd())
+				break;
+		}
+		for (it_operators = it_channel->second.getOperators().begin() ; it_operators != it_channel->second.getOperators().end() ; it_operators++)
+		{
+			if ((*it_operators) == it_guest->second.getFd())
+				break;
+		}
+		if (it_users == it_channel->second.getUsers().end() && it_operators == it_channel->second.getOperators().end())
+		{
+			std::vector<int>::iterator it_oper;
+			for (it_oper = it_channel->second.getOperators().begin() ; it_oper != it_channel->second.getOperators().end() ; it_oper++)
+				if ((*it_oper) == client.getFd())
+					break;
+			if (it_oper == it_channel->second.getOperators().end())
+			{
+				msg = ":localhost 482 " + client.getNick() + " " + channel + " :You're not channel operator\r\n";
+				if (send(client.getFd(), msg.c_str(), msg.size(), 0) < 1)
+					del_user(client.getFd());
+				return false;
+			}
+			else
+			{
+				addToWhitelist(it_channel, it_guest, client, channel);
+				return false;
+			}
+		}
+		else
+		{
+			msg = ":localhost 443 " + client.getNick() + " " + it_guest->second.getNick() + " :#" + channel + " :is already on channel\r\n";
+			if (send(client.getFd(), msg.c_str(), msg.size(), 0) < 1)
+			{
+				del_user(client.getFd());
+				return false;
+			}
+			return false;
+		}
 	}
 	else
-		//exception ou msg user already in channel
-		;
+	{
+		msg = ":localhost 403 " + client.getNick() + " " + channel + " :No such channel\r\n";
+		if (send(client.getFd(), msg.c_str(), msg.size(), 0) < 1)
+			del_user(client.getFd());
+		return false;
+	}
 	return true;
 }
